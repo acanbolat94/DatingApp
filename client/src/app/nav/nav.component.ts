f41c2a5ed0086e7732af42b258bb6f1e091380c7
Ahmet Can BOLAT <acanbolat@outlook.com>
2023-12-01 19:06:59 +0300

    Angular Routing Guards,Bootstrap Theme Complete

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,22 +15,30 @@ export class NavComponent implements OnInit {
   // represent is an observable
   // currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
+      // if we don't need a response we can use this syntax _ ()
+      next: (_) => {
+        this.router.navigateByUrl('/members');
       },
       error: (err) => {
-        console.log(err);
+        // toastr is applied here
+        this.toastr.error(err.error);
       },
     });
   }
 
   logout() {
     this.accountService.logout();
+    // when the user is log out the page will be redirect to home page
+    this.router.navigateByUrl('/');
   }
 }
